8bc7ccd681f6c1bb251229b3df675954f32b9b90
Cookie726 <2544967914@qq.com>
2024-04-09 22:03:30 +0800

    feat: parallel tasks

--- mytest/server.py
+++ mytest/server.py
@@ -13,24 +13,42 @@ import threading
 # 创建一个 Flask 应用程序
 app = Flask(__name__)
 
+class State:
+    def __init__(self):
+        self.stop_everything = False
 
 class Task:
     def __init__(self, user_input, stream):
         self.user_input = user_input
         self.priority = None
         self.stream = stream
-        self.need_save_content = False  # 需要保存中间值
+        self.need_save_content = False  #需要保存中间值
         self.is_done = False  # 是否完成任务
         self.is_preempt = False  # 是否发生了抢占
+        self.pre_output = ''
+        self.cache = ''
+        self.state = State()
+
+    def done(self):
+        self.is_done = True
+
+    def stop(self):
+        print('stop')
+        self.state.stop_everything = True
+
+    def is_stop(self):
+        return self.state.stop_everything
 
 
 class MLFQ:
+    cur_task: Task | None
     def __init__(self, num_queues, time_slice):
         self.num_queues = num_queues
         self.queues = [Queue() for _ in range(num_queues)]
         self.time_slice = time_slice
         self.lock = Lock()
         self.lock_timer = None  # 锁的计时器
+        self.cur_task = None
 
     def add_task(self, task):
         tokens_len = len(encode(task.user_input)[0])
@@ -52,16 +70,45 @@ class MLFQ:
 
     def save_content_no_preempt(self, task):
         task.need_save_content = True
-
-    def save_content_preempt(self, task):
-        task.need_save_content = True
-        task.is_preempt = True
+        self.stop_task()
 
     def degraded(self, task):
         if task.priority == self.num_queues-1:
             self.queues[task.priority].put(task)  # 已经是最低优先级：放回队尾，不降级
         else:
             self.queues[task.priority+1].put(task)  # 降级处理
+
+    def run_task(self, task: Task):
+        self.cur_task = task
+        self.cur_task.state.stop_everything = False
+        for value in start_chat(task, shared.state, shared.stopping_strings, True):
+            temp: str = value['visible'][-1][1]
+            task.cache = task.cache + temp.replace(task.pre_output, '', 1)
+            task.pre_output = temp
+        if not self.cur_task.is_stop():
+            task.done()
+            self.release()
+
+    def release(self):
+        if self.lock_timer:
+            self.lock_timer.cancel()
+        self.lock.release()
+
+    def save_content_preempt(self):
+        self.cur_task.need_save_content = True
+        self.cur_task.is_preempt = True
+        self.stop_task()
+
+    def stop_task(self):
+        # 暂停当前任务
+        self.cur_task.stop()
+        if (self.cur_task.is_preempt):
+            # 当前任务未执行完毕，放回队列
+            self.queues[self.cur_task.priority].put(self.cur_task)
+        else:
+            self.degraded(self.cur_task)
+            # 释放锁，自动执行下一个任务
+            self.release()
 
     def run(self):
         while True:
@@ -72,14 +119,14 @@ class MLFQ:
                     time.sleep(1)
                 # 有新 task + 有空执行
                 elif task is not None:
-                    print("locked")
                     self.lock.acquire()
                     # 启动锁的计时器
                     self.lock_timer = Timer(self.time_slice[priority], lambda: self.save_content_no_preempt(task))
                     self.lock_timer.start()
                     start_time = time.time()
                     # 执行当前任务
-                    task.stream = start_chat(task.user_input, shared.state, shared.stopping_strings, True)
+                    self.run_task(task)
+                    # task.stream = start_chat(task.user_input, shared.state, shared.stopping_strings, True)
                     # 检查锁在locked情况下，是否有更高优先级任务到来
                     while self.lock.locked():
                         if self.has_higher_priority_task(priority):
@@ -87,40 +134,26 @@ class MLFQ:
                             done_time = time.time() - start_time
                             # 执行完最小时间片，直接抢占
                             if done_time >= min_time_slice:
-                                self.lock_timer.cancel()  # 取消计时器
-                                self.save_content_preempt(task)
+                                self.save_content_preempt()
                             else:
                                 remaining_time = min_time_slice - done_time
                                 self.lock_timer.cancel()  # 取消计时器
                                 # 启动锁的计时器
-                                self.lock_timer = Timer(self.time_slice[priority], lambda: self.save_content_preempt(task))
+                                self.lock_timer = Timer(remaining_time, lambda: self.save_content_preempt())
                                 self.lock_timer.start()
 
 
 # 生成event-stream
-def generate(task, mlfq):
-    try:
-        for value in task.stream:
-            if task.need_save_content:
-                task.user_input = task.user_input + value['visible'][-1][1]  # 保存内容
-                task.stream = None
-                if task.is_preempt:
-                    mlfq.queues[task.priority].put(task)
-                else:
-                    mlfq.degraded(task)  # 降级处理
-                break
-            yield f"data: {value['visible'][-1][1]}\n\n"
-        yield "\n"
-        if not task.need_save_content:
-            task.is_done = True
-        task.need_save_content = False
-    finally:
-        # 在生成器完成时:清空计时器 + 释放锁
-        if mlfq.lock_timer:
-            mlfq.lock_timer.cancel()
-        mlfq.lock.release()
-        print("released")
-
+def generate(task: Task):
+    while True:
+        if not task.is_done or task.cache:
+            data = task.cache.replace("\n", ' ', -1)
+            yield f'data: Event: {data}\n\n'
+            task.cache = ''
+        else:
+            yield '\n\n'
+            break
+        time.sleep(1)
 
 # 加载模型：默认llama2-7b 4bit量化版本
 def load_llama_model():
@@ -146,13 +179,7 @@ def chat():
             # 创建任务对象
             task = Task(user_input, None)
             mlfq.add_task(task)
-            print("add")
-            while True:
-                if task.stream is not None:
-                    generate(task, mlfq)
-                if task.is_done:
-                    break
-            return ''
+            return Response(stream_with_context(generate(task)), content_type='text/event-stream')
         else:
             return Response('请输入文本')
     else:
